03ff8e0c56a1eefc3391b03e5f08c99c63f71d3d
Steven Pecht <steven.pecht@gmail.com>
2017-12-29 16:45:39 -0500

    Fixed bug with death animation spawning in wrong spot

--- game/components/health.gd
+++ game/components/health.gd
@@ -17,7 +17,7 @@ func set_health(new_health):
 	health = new_health
 	if health <= 0:
 		var death_anim = scn_death_animation.instance()
-		death_anim.set_pos(get_pos())
+		death_anim.set_pos(get_parent().get_pos())
 		var game_node = get_node('/root/game')
 		game_node.add_child(death_anim)
 		get_parent().queue_free()
